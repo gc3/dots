3ed5d14582f97ab1aff4671795dcc0ef30c5542a
gc3 <gc3@users.noreply.github.com>
2023-03-15 20:34:47 -0700

    use __main__ and add a setup for py2app so you can run dots on a mac as a normal application

--- README.md
+++ README.md
@@ -7,6 +7,10 @@ Just click on dots. If they are the same color and next to each other, you can s
 
 
 ## Requirements
-
 - python 3
 - wx python UI library ([www](https://www.wxpython.org/), [github](https://github.com/wxWidgets/Phoenix/))
+
+## Run on MacOS
+Using [py2app](https://py2app.readthedocs.io/en/latest/index.html), you can run the following command in your code checkout to produce a dots.app/ that MacOS can run as what appears to be a native app.
+
+    python3.10 setup.py py2app --iconfile images/dots.ico -O2
--- main.py
+++ main.py
@@ -16,30 +16,34 @@ import util
 PROGRAM_NAME  = "Dots by gc3"
 DEFAULT_MOVES = 20
 
-# Parse any command line arguments
-parser = argparse.ArgumentParser(
-  prog=PROGRAM_NAME,
-  description="A fun game of removing dots for high scores!"
-)
-parser.add_argument("-m", "--moves",  type=int, default=DEFAULT_MOVES)
-args = parser.parse_args()
+def main():
+  # Parse any command line arguments
+  parser = argparse.ArgumentParser(
+    prog=PROGRAM_NAME,
+    description="A fun game of removing dots for high scores!"
+  )
+  parser.add_argument("-m", "--moves",  type=int, default=DEFAULT_MOVES)
+  args = parser.parse_args()
 
-if __debug__:
-  print ("Commandline Arguments:\n" + " " + str(args))
+  if __debug__:
+    print ("Commandline Arguments:\n" + " " + str(args))
 
-# Create a new app, don't redirect stdout/stderr to a window.
-app = wx.App(False)
+  # Create a new app, don't redirect stdout/stderr to a window.
+  app = wx.App(False)
 
-# game logic fed to the UI so it can react
-game = game.DotsGame(max_moves=args.moves)
+  # game logic fed to the UI so it can react
+  gamel = game.DotsGame(max_moves=args.moves)
 
-# Throw up a splash screen while the game loads
-util.createSplashScreen()
+  # Throw up a splash screen while the game loads
+  util.createSplashScreen()
 
-# instantiate the ui
-frame = ui.DotsFrame(None, wx.ID_ANY, PROGRAM_NAME, game)
-frame.Centre()
-frame.Show(True)
+  # instantiate the ui
+  frame = ui.DotsFrame(None, wx.ID_ANY, PROGRAM_NAME, gamel)
+  frame.Centre()
+  frame.Show(True)
 
-# go go go!
-app.MainLoop()
+  # go go go!
+  app.MainLoop()
+
+if __name__ == '__main__':
+    main()
